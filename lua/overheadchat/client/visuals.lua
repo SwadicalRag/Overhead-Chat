f20f762b156a938cee4f88d1d91c6709324d4740
Swad <SwadicalRag@users.noreply.github.com>
2015-12-28 11:01:36 +1100

    Minor visuals tweak

--- lua/overheadchat/client/visuals.lua
+++ lua/overheadchat/client/visuals.lua
@@ -1,7 +1,17 @@
 local chatIcon = Material("icon16/comment.png","noclamp smooth")
 
+OverheadChat.Rainbow = false
+
 hook.Add("PostDrawTranslucentRenderables","swadical.OverheadChat.Draw",function()
-    local font = "DermaDefault"
+    local font = "DermaLarge"
+    local textDrawCol,backgroundDrawCol
+
+    if OverheadChat.Rainbow then
+        textDrawCol,backgroundDrawCol = HSVToColor((SysTime()*100)%360,0.65,0.9),Color(255,255,255,50)
+    else
+        textDrawCol,backgroundDrawCol = Color(0,0,0,240),Color(255,255,255,50)
+    end
+
     for _,ply in ipairs(player.GetAll()) do
         local text = ply:GetChatStore():GetText()
         if text == "" then continue end
@@ -12,15 +22,18 @@ hook.Add("PostDrawTranslucentRenderables","swadical.OverheadChat.Draw",function(
         ang.r = ang.r + 90
 
 
-        cam.Start3D2D(ply:GetPos() + Vector(0,0,85),ang,1)
+        cam.Start3D2D(ply:GetPos() + Vector(0,0,85),ang,0.1)
         	surface.SetFont(font)
         	local w,h = surface.GetTextSize(text)
-            local extra = 14
-            draw.RoundedBox(1,-w/2-extra,0,w+extra + 1,h,Color(0,0,0,150))
-            draw.DrawText(text,font,0,-1,Color(255,255,255),TEXT_ALIGN_CENTER)
+            local padding = 30
+            local extra = 10
+            render.PushFilterMag(TEXFILTER.ANISOTROPIC)
+                draw.RoundedBox(1,-w/2-padding,0,w+padding + extra,h,backgroundDrawCol)
+                draw.DrawText(text,font,4,-1,textDrawCol,TEXT_ALIGN_CENTER)
+            render.PopFilterMag(TEXFILTER.ANISOTROPIC)
             surface.SetMaterial(chatIcon)
             surface.SetDrawColor(255,255,255)
-	        surface.DrawTexturedRect(-13-w/2,(h-h/1.1),h/1.1,h/1.1)
+	        surface.DrawTexturedRect(-padding+1-w/2,(h-h/1.1),h/1.1,h/1.1)
         cam.End3D2D()
     end
 end)
